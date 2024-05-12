950bf372091d061429dfccd706e030dbc4849cd6
Amir <herus2006cripto.gmail.com>
2024-05-12 14:01:34 +0330

    comment streamClientProvider component for building

--- providers/streamClientProvider.tsx
+++ providers/streamClientProvider.tsx
@@ -1,30 +1,30 @@
-'use client'
+// 'use client'
 
-import { useUser } from '@clerk/nextjs';
-import { StreamVideo , StreamVideoClient  } from '@stream-io/video-react-sdk';
-import React, { useEffect, useState } from 'react';
+// import { useUser } from '@clerk/nextjs';
+// import { StreamVideo , StreamVideoClient  } from '@stream-io/video-react-sdk';
+// import React, { useEffect, useState } from 'react';
 
-const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+// const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
-export const StreamClientProvider = ({children} : {children : React.ReactNode}) => {
-  const [videoClient , setVideoClient] = useState<StreamVideoClient>()
-  const {user , isLoaded} = useUser()
+// export const StreamClientProvider = ({children} : {children : React.ReactNode}) => {
+//   const [videoClient , setVideoClient] = useState<StreamVideoClient>()
+//   const {user , isLoaded} = useUser()
 
-  useEffect(() => {
-    if(!user || !isLoaded) return;
-    if(!apiKey) throw Error('we have missed api key!')
-  } , [user , isLoaded])
+//   useEffect(() => {
+//     if(!user || !isLoaded) return;
+//     if(!apiKey) throw Error('we have missed api key!')
+//   } , [user , isLoaded])
 
-  const client = new StreamVideoClient({
-    user : {
-      id : 'user?.id',
-      name : user?.username || user?.id,
-      image : user?.imageUrl ,
-    },
-    tokenProvider : ''
-  })
-  return (
-    <StreamVideo client={videoClient}>
-    </StreamVideo>
-  );
-};
+//   const client = new StreamVideoClient({
+//     user : {
+//       id : 'user?.id',
+//       name : user?.username || user?.id,
+//       image : user?.imageUrl ,
+//     },
+//     tokenProvider : ''
+//   })
+//   return (
+//     <StreamVideo client={videoClient}>
+//     </StreamVideo>
+//   );
+// };
