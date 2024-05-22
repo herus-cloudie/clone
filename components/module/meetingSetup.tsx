f029202e969ff984c02b290715efe800196a24d6
Amir <herus2006cripto.gmail.com>
2024-05-22 10:16:46 +0330

    get responsive meetingSetup

--- components/module/meetingSetup.tsx
+++ components/module/meetingSetup.tsx
@@ -31,7 +31,7 @@ const MeetingSetup = ({setIsSetupComplete} : {setIsSetupComplete: (value: boolea
   return (
     <div className="flex justify-center items-center flex-col gap-3 w-full h-screen">
       <h2 className="mb-3 font-semibold text-3xl">تنظیمات پیش‌فرض</h2>
-      <VideoPreview />
+      <VideoPreview className="w-[300px] h-[190px] sm:w-[500px] sm:h-[375px]"/>
       <div className="flex gap-16 mt-6">
         <div className="font-sans"><DeviceSettings /></div>
         <div className="flex items-center"><input onChange={() => setMicrophone(!microphone)} type="checkbox"/><label className="mr-2 font-medium">میکروفون</label></div>
@@ -44,3 +44,12 @@ const MeetingSetup = ({setIsSetupComplete} : {setIsSetupComplete: (value: boolea
 
 export default MeetingSetup;
 
+    //const [members, setMembers] = useState([])
+    // const user = useConnectedUser();
+    //  const saveMember = await fetch('/api/saveMember' , {
+    //   method : 'POST',
+    //   body : JSON.stringify({user , callId : call.id}),
+    //   headers : {'Content-Type': 'application/json'}
+    // })
+    // const Data = await saveMember.json();
+    // console.log(Data , user)
