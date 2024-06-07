1b2e15ea4ae42c8c76112db5f8ee7e2125d34e91
Amir <herus2006cripto.gmail.com>
2024-06-07 13:24:33 +0330

    style sheet

--- components/module/meetingSetup.tsx
+++ components/module/meetingSetup.tsx
@@ -41,7 +41,7 @@ const MeetingSetup = ({setIsSetupComplete} : {setIsSetupComplete: (value: boolea
   return (
     <div className="flex justify-center items-center flex-col gap-3 w-full h-screen">
       <h2 className="mb-3 font-semibold text-3xl">تنظیمات پیش‌فرض</h2>
-      <VideoPreview className="w-[300px] h-[190px] sm:w-[500px] sm:h-[375px]"/>
+      <VideoPreview className="video_preview"/>
       <div className="flex gap-16 mt-6">
         <div className="font-sans"><DeviceSettings /></div>
         <div className="flex items-center"><input onChange={() => setMicrophone(!microphone)} type="checkbox"/><label className="mr-2 font-medium">میکروفون</label></div>
@@ -49,7 +49,7 @@ const MeetingSetup = ({setIsSetupComplete} : {setIsSetupComplete: (value: boolea
       </div>
       <div className="flex justify-around items-end gap-5">
         <Button className="rounded-md bg-green-500 px-4 py-2.5 text-xl mt-5" onClick={joining}>ملحق شدن</Button>
-        <Button onClick={() => setIsDialogOpen(true)} className="bg-dark-4 px-4 py-2.5 text-xl mt-5 rounded-md">دعوت<img alt="group icon" style={{ filter: 'invert(1)' }} className="mr-1" src='/icons/group.png' width={25} height={25} /></Button>
+        <Button onClick={() => setIsDialogOpen(true)} className="bg-dark-4 px-4 py-2.5 text-xl mt-5 rounded-md">دعوت<img alt="group icon" style={{ filter: 'invert(1)' }} className="mr-1" src='/icons/group.png' width={25} height={2} /></Button>
       </div>
       <SendMeetingRequest callId={call.id} setIsDialogOpen={setIsDialogOpen} isDialogOpen={isDialogOpen}/>
     </div>
